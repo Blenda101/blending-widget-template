ee0f91ebdd3bba52e58c1b3bf3a626173592d05a
WahidHoquee <wahidhoquee@gmail.com>
2023-07-31 14:26:33 +0600

    Fixed the scss compiler during dev mode

--- server/dev.js
+++ server/dev.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
-const sassMiddleware = require('node-sass-middleware')
 const livereload = require('livereload');
 const connectLiveReload = require('connect-livereload');
 
 var wiki = require('./data/wiki');
 var config = require('../config.json');
+const { compileSass } = require('./utils/compiler');
 
 var app = express();
 
@@ -20,15 +20,12 @@ liveReloadServer.server.once('connection', () => {
 app.use(connectLiveReload());
 
 // Setting custom directory for rendering ejs files
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, '../styles'),
-    dest: path.join(__dirname, '/public/'),
-    outputStyle: 'compressed',
-  })
-)
+app.use((req, res, next) => {
+  compileSass('./server/public', 'style.css', next);
+});
+
 app.use(express.static(path.join(__dirname, '../assets')));
-// app.use(express.static(path.join(__dirname, '../styles')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, '../server/public')));
 app.set('views', path.join(__dirname, './views'));
 
@@ -36,7 +33,7 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {meta: config, data: wiki, methods: {} });
+  res.render('index', { meta: config, data: wiki, methods: {} });
 });
 
 app.listen(8080, () => {
--- server/utils/compiler.js
+++ server/utils/compiler.js
@@ -1,22 +1,27 @@
 var sass = require('node-sass');
 const fs = require('fs');
 
-const compileSass = (DIRECTORY) => {
-  sass.render({
-    file: 'styles/style.scss',
-    outputStyle: 'compressed',
-  }, (error, result) => {
-    if(error){
-      console.log(error)
-    }
-    fs.writeFile(`${DIRECTORY}/main.css`, result.css, function(err){
-      if(err){
-        console.log(err)  
+const compileSass = (DIRECTORY, FILENAME = 'main.css', next) => {
+  sass.render(
+    {
+      file: 'styles/style.scss',
+      outputStyle: 'compressed',
+    },
+    (error, result) => {
+      if (error) {
+        console.log(error);
       }
-    });
-  });
-}
+      fs.writeFile(`${DIRECTORY}/${FILENAME}`, result.css, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          if (next) next();
+        }
+      });
+    },
+  );
+};
 
 module.exports = {
-  compileSass
-}
+  compileSass,
+};
